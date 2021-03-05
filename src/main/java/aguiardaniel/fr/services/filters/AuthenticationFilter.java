94b2c441abb35090363a87e055cae832e8fbabde
Daniel Aguiar <daniel.aguiar@outlook.fr>
2021-03-05 17:33:17 +0100

    fix: error on load

--- src/main/java/aguiardaniel/fr/services/filters/AuthenticationFilter.java
+++ src/main/java/aguiardaniel/fr/services/filters/AuthenticationFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
